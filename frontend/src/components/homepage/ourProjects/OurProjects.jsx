af425fe17031ca3fd616fef3ecd763313bbe2dc3
NADAMAHer74 <nada01141089562@gmail.com>
2024-12-24 03:07:45 +0200

    updates

--- frontend/src/components/homepage/ourProjects/OurProjects.jsx
+++ frontend/src/components/homepage/ourProjects/OurProjects.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import portofolio from "../../../assets/portfolio_351456 1.png";
 import { Swiper, SwiperSlide } from "swiper/react";
+import project1 from "../../../assets/GameWorrier.png";
+import project2 from "../../../assets/AutoNet.png";
+import project3 from "../../../assets/gem-gym.png";
+import project4 from "../../../assets/movies.png";
+import project5 from "../../../assets/tradex.png";
+import cloneProject1 from "../../../assets/Chat-App-Clone.png";
+import cloneProject2 from "../../../assets/Netflix-Clone.png";
+import cloneProject3 from "../../../assets/Threads-Clone.png";
+import cloneProject4 from "../../../assets/E-Commerce-Clone.png";
+import cloneProject5 from "../../../assets/travello-clone.png";
 
 // Import Swiper styles
 import "swiper/css";
@@ -11,20 +21,20 @@ import "./styles.css";
 import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
 
 export default function OurProjects() {
-  const clonedProjectsSlides = [
-    { image: "https://swiperjs.com/demos/images/nature-1.jpg" },
-    { image: "https://swiperjs.com/demos/images/nature-2.jpg" },
-    { image: "https://swiperjs.com/demos/images/nature-5.jpg" },
-    { image: "https://swiperjs.com/demos/images/nature-7.jpg" },
-    { image: "https://swiperjs.com/demos/images/nature-8.jpg" },
+  const templatesSlides = [
+    { image: `${project1}`, link: "https://game-worrier.vercel.app/" },
+    { image: `${project2}`, link: "https://autonet.netlify.app/" },
+    { image: `${project4}`, link: " https://movies-clone.onrender.com/" },
+    { image: `${project3}`, link: "https://gem-gym.onrender.com/" },
+    { image: `${project5}`, link: "https://tradex.onrender.com/" },
   ];
 
-  const templatesSlides = [
-    { image: "https://swiperjs.com/demos/images/nature-3.jpg" },
-    { image: "https://swiperjs.com/demos/images/nature-4.jpg" },
-    { image: "https://swiperjs.com/demos/images/nature-6.jpg" },
-    { image: "https://swiperjs.com/demos/images/nature-9.jpg" },
-    { image: "https://swiperjs.com/demos/images/nature-10.jpg" },
+  const clonedProjectsSlides = [
+    { image: `${cloneProject1}` },
+    { image: `${cloneProject2}` },
+    { image: `${cloneProject5}` },
+    { image: `${cloneProject3}` },
+    { image: `${cloneProject4}` },
   ];
   const [slides, setSlides] = useState(clonedProjectsSlides);
   const [activeButton, setActiveButton] = useState("cloned");
@@ -44,36 +54,35 @@ export default function OurProjects() {
       <div className="flex flex-col justify-center items-center my-20">
         <img src={portofolio} alt="Portofolio" />
         <h2 className="text-3xl font-bold text-sky-700">Our Projects</h2>
-        <p className="text-center my-4 w-11/12 md:w-full text-xl md:text-2xl  font-bold">
+        <p className="text-center w-11/12 md:w-1/2 my-6 font-bold text-gray-600">
           A Showcase of Expertly Crafted Template-Based Projects,
-          Custom-Designed and Tailored to Fit the
-          <br /> Unique Requirements of Each Business, Ensuring Quality and
-          Efficiency
+          Custom-Designed and Tailored to Fit the Unique Requirements of Each
+          Business, Ensuring Quality and Efficiency
         </p>
-        <div className="flex mt-12 ">
-          <button
-            onClick={() => handleButtonClick(clonedProjectsSlides, "cloned")}
-            type="button"
-            className={`text-xl font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ${
-              activeButton === "cloned"
-                ? "bg-sky-700 text-white"
-                : "text-sky-700 bg-white"
-            } hover:bg-sky-700 hover:text-white focus:outline-none  `}
-          >
-            Cloned Projects
-          </button>
-          <button
-            onClick={() => handleButtonClick(templatesSlides, "templates")}
-            type="button"
-            className={`text-xl font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ${
-              activeButton === "templates"
-                ? "bg-sky-700 text-white"
-                : "text-sky-700 bg-white"
-            } hover:bg-sky-700 hover:text-white focus:outline-none  `}
-          >
-            Templates
-          </button>
-        </div>
+      </div>
+      <div className="flex mt-12  justify-center">
+        <button
+          onClick={() => handleButtonClick(clonedProjectsSlides, "cloned")}
+          type="button"
+          className={`text-xl font-medium rounded-lg  px-5 py-2.5 text-center me-2 mb-2 ${
+            activeButton === "cloned"
+              ? "bg-sky-700 text-white"
+              : "text-sky-700 bg-white"
+          } hover:bg-sky-700 hover:text-white focus:outline-none  `}
+        >
+          Cloned Projects
+        </button>
+        <button
+          onClick={() => handleButtonClick(templatesSlides, "templates")}
+          type="button"
+          className={`text-xl font-medium rounded-lg  px-5 py-2.5 text-center me-2 mb-2 ${
+            activeButton === "templates"
+              ? "bg-sky-700 text-white"
+              : "text-sky-700 bg-white"
+          } hover:bg-sky-700 hover:text-white focus:outline-none  `}
+        >
+          Templates
+        </button>
       </div>
       <Swiper
         effect={"coverflow"}
@@ -91,11 +100,16 @@ export default function OurProjects() {
           clickable: true,
         }}
         modules={[EffectCoverflow, Navigation, Pagination]}
-        className="mySwiper"
+        className="!w-full !p-0 !my-12"
       >
         {slides.map((slide, index) => (
-          <SwiperSlide key={index}>
-            <img src={slide.image} alt={`Slide ${index + 1}`} />
+          <SwiperSlide key={index} className="!max-w-[400px]  !h-[400px] !mt-0">
+            <img
+              src={slide.image}
+              alt={`Slide ${index + 1}`}
+              className="!w-full !h-[330px] !border !rounded-xl"
+              onClick={() => window.open(slide.link, "_blank")}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
--- frontend/src/pages/OurProjectsPage.jsx
+++ frontend/src/pages/OurProjectsPage.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React, { useState } from "react";
+import Body from "../components/Body";
+import Heading from "../components/Heading";
+import ourProjectsBg from "../assets/2150010115 1.png";
+import Pagination from "../components/Pagination";
+import Cards from "../components/Cards";
+import project1 from "../assets/GameWorrier.png";
+import project2 from "../assets/AutoNet.png";
+import project3 from "../assets/gem-gym.png";
+import project4 from "../assets/movies.png";
+import project5 from "../assets/tradex.png";
+import cloneProject1 from "../assets/Chat-App-Clone.png";
+import cloneProject2 from "../assets/Netflix-Clone.png";
+import cloneProject3 from "../assets/Threads-Clone.png";
+import cloneProject4 from "../assets/E-Commerce-Clone.png";
+import cloneProject5 from "../assets/travello-clone.png";
 
 const OurProjectsPage = () => {
-  return (
-    <div>OurProjectsPage</div>
-  )
-}
+  const templateCardData = [
+    {
+      id: 1,
+      title: "GameWorrier",
+      imageUrl: `${project1}`,
+      link: "https://diamond-games.onrender.com/",
+    },
+    {
+      id: 2,
+      title: "Movies",
+      imageUrl: `${project4}`,
+      link: "https://moviessimplewebsite.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "AutoNet",
+      imageUrl: `${project2}`,
+      link: "https://autonet.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "Gem-Gym",
+      imageUrl: `${project3}`,
+      link: "https://gem-gym.onrender.com/",
+    },
+    {
+      id: 5,
+      title: "Tradex",
+      imageUrl: `${project5}`,
+      link: "https://nadamaher74.github.io/Tradex/",
+    },
+  ];
+  const clonedProjectsCardData = [
+    {
+      id: 1,
+      title: "Chat-App",
+      imageUrl: `${cloneProject1}`,
+    },
+    {
+      id: 2,
+      title: "Netflix",
+      imageUrl: `${cloneProject2}`,
+    },
 
-export default OurProjectsPage
+    {
+      id: 3,
+      title: "Travello",
+      imageUrl: `${cloneProject5}`,
+    },
+    { id: 4, title: "Threads", imageUrl: `${cloneProject3}` },
+    {
+      id: 5,
+      title: "E-Commerce",
+      imageUrl: `${cloneProject4}`,
+    },
+  ];
+  const cardsPerPage = 3;
+  const [currentTemplatePage, setCurrentTemplatePage] = useState(0);
+  const startTemplateIdx = currentTemplatePage * cardsPerPage;
+  const currentTemplateCards = templateCardData.slice(
+    startTemplateIdx,
+    startTemplateIdx + cardsPerPage
+  );
+
+  const [currentClonedPage, setCurrentClonedPage] = useState(0);
+  const startClonedIdx = currentClonedPage * cardsPerPage;
+  const currentClonedCards = clonedProjectsCardData.slice(
+    startClonedIdx,
+    startClonedIdx + cardsPerPage
+  );
+
+  return (
+    <>
+      <Heading
+        bgImg={ourProjectsBg}
+        title="OUR PROJECTS"
+        subtitle="A Showcase of Tailored Solutions, Innovative Designs, and Seamless Functionality Built to Empower Businesses and Ideas"
+      />
+      <Body>
+        <h1 className="text-4xl  my-10 font-bold text-center text-blue-600">
+          TEMPLATES
+        </h1>
+        <Cards currentCards={currentTemplateCards} />
+        <Pagination
+          currentPage={currentTemplatePage}
+          setCurrentPage={setCurrentTemplatePage}
+          totalCards={templateCardData.length}
+          cardsPerPage={cardsPerPage}
+        />
+        <h1 className="text-4xl  my-10 font-bold text-center text-blue-600">
+          CLONED PROJECTS
+        </h1>
+        <Cards currentCards={currentClonedCards} />
+        <Pagination
+          currentPage={currentClonedPage}
+          setCurrentPage={setCurrentClonedPage}
+          totalCards={clonedProjectsCardData.length}
+          cardsPerPage={cardsPerPage}
+        />
+      </Body>
+    </>
+  );
+};
+
+export default OurProjectsPage;
