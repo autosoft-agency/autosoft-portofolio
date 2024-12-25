012f05012adf36fe39bbdc8a6d2b0307c6f5147a
Mohamed-Kassab-96 <mohamedhosny6055@gmail.com>
2024-12-25 04:58:42 +0200

    about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Heading from '../components/Heading'
+import aboutBg from '../assets/about-bg.png';
+import AboutContent from '../components/about/AboutContent';
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <Heading
+        bgImg={aboutBg}
+        title="ABOUT US"
+        subtitle="We are a team of experienced developers and designers who specialize in creating user-friendly and visually stunning websites and applications." 
+      />
+      <AboutContent />
+    </div>
   )
 }
 
