aec7c96070e04ec74ab2ad4b77dc95743390587f
NADAMAHer74 <nada01141089562@gmail.com>
2024-12-30 21:39:06 +0200

    updates

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -3,20 +3,20 @@ import React from "react";
 function Cards({ currentCards }) {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
         {currentCards.map((card) => (
           <div
             key={card.id}
-            className="card w-full col-span-1 bg-base-100 image-full shadow-xl group"
+            className="card  relative col-span-1  shadow-xl group md:w-[450px] md:h-[250px] "
           >
-            <figure className="transition-transform duration-400 ease-in-out overflow-hidden">
+            <figure className=" absolute top-0 left-0 size-full border rounded-2xl  transform transition-transform duration-400 ease-in-out overflow-hidden">
               <img
                 src={card.imageUrl}
                 alt={card.title}
-                className="transform transition-transform duration-400 ease-in-out group-hover:scale-105"
+                className="size-full transform transition-transform duration-500 group-hover:scale-105"
               />
             </figure>
-            <div className="card-body  border rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out bg-black bg-opacity-60 !text-white font-bold">
+            <div className="card-body  border rounded-2xl opacity-0 group-hover:opacity-100  transition-opacity duration-1000  bg-black bg-opacity-60 !text-white font-bold">
               <h2 className="card-title font-semibold m-auto text-3xl">
                 {card.title}
               </h2>
